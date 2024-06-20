f6427560cba5c58f0c42e33fcaa28177623b0e84
Ahmed AbdELMajeed Hussein <88937645+Aabdelmajeed@users.noreply.github.com>
2024-06-20 20:31:42 +0300

    first commit

---
+++ src/main/java/com/outbox/MsArchitecture/MsArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.outbox.MsArchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outbox/MsArchitecture/MsArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outbox.MsArchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
